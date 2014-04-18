b20431d381d3466e462c122594531327149289b4
Greg Bowser <topnotcher@gmail.com>
2014-04-18 09:38:55 -0400

    configure the flashing red light to receive notifications.
    In shinken, the redlight gets configured as a notificationway that sends
    a simple HTTP request to to the Raspberry pi. When a DOWN or CRITICAL
    notification is received, it is saved in a dict with the timestamp.
    After the state remains for 30 minutes, or another state notification
    (UP, WARNING, OKAY, UKNOWN) is received, the light is turned off.
    
    There is also a web page included to facilitate manual activation of the
    red light in times of emergency!

--- monitor.py
+++ monitor.py
@@ -1,12 +1,68 @@
 #!/usr/bin/env python2
 import RPi.GPIO as gpio
-import sys,json
-import time
-import logging
-import urllib2
+import time, threading
+from flask import Flask, render_template, request
 
-SHINKEN_URL = 'http://mig.app.security.uri.edu/shinken.php'
 LIGHT_GPIO = 2
+CRITICAL_TIMEOUT = 1800
+
+app = Flask(__name__)
+
+class WatcherThread(threading.Thread):
+    def __init__(self,states):
+        threading.Thread.__init__(self)
+        self.states = states
+    def run(self):
+        while True:
+            time.sleep(30)
+            manage_sexy_light(self.states)
+
+class ServiceStates(object):
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.states = {}
+
+    def lock_states(self):
+        self.lock.acquire()
+
+    def release_states(self):
+        self.lock.release()
+
+    def clear_critical(self,key):
+        self.lock_states()
+
+        if key in self.states:
+            self.states.pop(key)
+
+        self.release_states()
+
+    def set_critical(self,key):
+        self.lock_states()
+        self.states[key] = time.time()
+        self.release_states()
+
+    def update_state(self,key,state):
+        if state in ('CRITICAL','DOWN'):
+            self.set_critical(key)
+        else:
+            self.clear_critical(key)
+
+    def count_critical(self):
+        count = 0 
+        remove = []
+        self.lock_states()
+        for key,value in self.states.iteritems():
+            if time.time() - value > CRITICAL_TIMEOUT:
+                remove.append(key)
+            else:
+                count += 1
+
+        for key in remove:
+            self.states.pop(key)
+
+        self.release_states()
+        return count
+
 
 def setup_sexy_red_police_light():
 	gpio.setwarnings(False)
@@ -23,64 +79,34 @@ def disable_sexy_red_police_light():
 def toggle_sexy_red_police_light(on):
 	gpio.output(LIGHT_GPIO, on)
 
-def service_counts(services):
-	# this needs to be edited to reflect the actual values.
-	status_map = {
-		0: 'ok',
-		1: 'warning',
-		2: 'critical', 
-		3: 'unknown'
-	};
-
-	service_status = 1;
-
-	# set default counts to 0.
-	statuses = { 'total': 0 }
-	for id in status_map:
-		statuses[ status_map[id] ] = 0;
-
-	#add the counts 
-	for service in services:
-		statuses[ status_map[ service[service_status] ] ] += 1;
-		statuses['total'] += 1;
-
-	return statuses;	
-
-def monitor():
-	data = json.load(urllib2.urlopen(SHINKEN_URL))
-	statuses = {}
-
-	for host in data:
-		counts = service_counts(host[105])
-
-		for idx in counts:
-			if not idx in statuses:
-				statuses[idx] = 0
-			
-			statuses[idx] += counts[idx]
+def manage_sexy_light(states):
+    if states.count_critical() > 0:
+        flash_sexy_red_police_light()
+    else:
+        disable_sexy_red_police_light()
 
 
-	if statuses['critical'] > 0:
-		flash_sexy_red_police_light()
-	else:
-		disable_sexy_red_police_light()
+@app.route('/snafu')
+def snafu():
+    return render_template('snafu.html')
 
-
+@app.route('/')
 def main():
-	setup_sexy_red_police_light()
-	flash_sexy_red_police_light()
-	logging.basicConfig(leevl=logging.DEBUG)
+    key = request.args.get('key',None)
+    state = request.args.get('state','OK')
 
-	while True:
-		try:
-			monitor()
-		except Exception,e:
-			logging.exception(e)
-			flash_sexy_red_police_light()
-		finally:
-			time.sleep(120);
+    if key is not None:
+        states.update_state(key,state)
+    
+    manage_sexy_light(states)
 
-	flash_sexy_red_police_light()
+    return ''
+
 
 if __name__ == '__main__':
-	main()
+    setup_sexy_red_police_light()
+    states = ServiceStates()
+    WatcherThread(states).start()
+    app.run(host='0.0.0.0')
+
+
